dc687844ab180ae8acfec77895bf283b2f74d78b
Tydik <tgaraev653@gmail.com>
2024-08-02 21:15:52 +0300

    added pixel sampling

--- core/include/camera.h
+++ core/include/camera.h
@@ -7,6 +7,7 @@ class camera {
 public:
 	double aspect_ratio = 1.0;  // Ratio of image width over height
 	int    image_width  = 100;  // Rendered image width in pixel count
+	int    samples_per_pixel = 10;   // Count of random samples for each pixel
 
 	void render(const hittable& world);
 
@@ -16,8 +17,11 @@ private:
 	point3 pixel00_loc;    // Location of pixel 0, 0
 	vec3   pixel_delta_u;  // Offset to pixel to the right
 	vec3   pixel_delta_v;  // Offset to pixel below
+	double pixel_samples_scale; // Color scale factor for a sum of pixel samples
 
 	void initialize();
-	color ray_color(const ray& r, const hittable& world) const;
+	[[nodiscard]] color ray_color(const ray& r, const hittable& world) const;
+	[[nodiscard]] ray get_ray(int i, int j) const;
+	[[nodiscard]] vec3 sample_square() const;
 };
 }
--- core/src/color.cpp
+++ core/src/color.cpp
@@ -1,4 +1,5 @@
 #include "color.h"
+#include "interval.h"
 
 namespace simpleRT {
 void write_color(std::ostream& out, const color& pixel_color)
@@ -8,9 +9,10 @@ void write_color(std::ostream& out, const color& pixel_color)
 	auto b = pixel_color.get_z();
 
 	// Translate the [0,1] component values to the byte range [0,255].
-	int rbyte = int(255.999 * r);
-	int gbyte = int(255.999 * g);
-	int bbyte = int(255.999 * b);
+	static const interval intensity(0.000, 0.999);
+	int rbyte = int(256 * intensity.clamp(r));
+	int gbyte = int(256 * intensity.clamp(g));
+	int bbyte = int(256 * intensity.clamp(b));
 
 	// Write out the pixel color components.
 	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
